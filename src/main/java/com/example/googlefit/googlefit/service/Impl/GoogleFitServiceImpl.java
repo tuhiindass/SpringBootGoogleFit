713c3e76ca54ba0f5d0657c0ce9175297c452327
tuhiindass <tuhiindass@gmail.com>
2022-05-18 16:22:29 +0530

    First

--- src/main/java/com/example/googlefit/googlefit/service/Impl/GoogleFitServiceImpl.java
+++ src/main/java/com/example/googlefit/googlefit/service/Impl/GoogleFitServiceImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletResponse;
 
+import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateMidnight;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,6 +49,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 @Service
+@Slf4j
 public class GoogleFitServiceImpl implements GoogleFitServiceI{
 	
 	@Autowired
